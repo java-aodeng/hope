325ccc7d1f01880eec2416441f81200146b57121
java-aodeng <35907367+java-aodeng@users.noreply.github.com>
2018-07-19 21:27:44 +0800

    提交0.0.1

--- src/main/java/com/ad/core/main/Main.java
+++ src/main/java/com/ad/core/main/Main.java
@@ -2,6 +2,6 @@ package com.ad.core.main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world！");
     }
 }
