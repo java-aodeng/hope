c2e5448fbe0c968299b7fa491f304a96b38c5bc2
java-aodeng <35907367+java-aodeng@users.noreply.github.com>
2018-07-19 21:25:33 +0800

    提交0.0.1

--- src/main/java/com/ad/core/Application.java
+++ src/main/java/com/ad/core/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        System.out.println("SpringBoot2.1.0 Application run!");
+        System.out.println("SpringBoot2.1.0 Application Run!");
         SpringApplication.run(Application.class,args);
     }
 }
--- src/main/java/com/ad/core/controller/TeseController.java
+++ src/main/java/com/ad/core/controller/TeseController.java
@@ -4,4 +4,7 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class TeseController {
+    public void test(){
+        
+    }
 }
