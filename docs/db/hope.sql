b85dc7234f7a6cb3c341bc809174fc0f657c502c
java-aodeng <35907367+java-aodeng@users.noreply.github.com>
2018-09-18 14:59:58 +0800

    提交0.2.7 提交db文件

--- docs/db/hope.sql
+++ docs/db/hope.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50717
 File Encoding         : 65001
 
-Date: 2018-07-27 16:34:27
+Date: 2018-09-18 14:38:52
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -28,18 +28,19 @@ CREATE TABLE `sys_resource` (
   `type` int(1) NOT NULL COMMENT '资源类型:0目录 1菜单 2按钮',
   `priority` int(3) NOT NULL COMMENT '显示顺序',
   `parent_id` int(11) NOT NULL COMMENT '父编号',
-  `parent_ids` varchar(100) NOT NULL COMMENT '父编号列表',
-  `permission` varchar(100) NOT NULL COMMENT '权限字符串',
+  `permission` varchar(100) NOT NULL COMMENT '权限标识',
   `icon` varchar(50) DEFAULT '0' COMMENT '图标',
-  `available` int(1) NOT NULL COMMENT '是否可用:1有效2删除',
+  `status` int(1) NOT NULL COMMENT '是否可用:1有效2删除',
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   `update_time` datetime DEFAULT NULL COMMENT '修改时间',
   PRIMARY KEY (`resourceId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of sys_resource
 -- ----------------------------
+INSERT INTO `sys_resource` VALUES ('1', '1', '工作台', '工作台', '/workdest', '1', '1', '0', 'workdest', 'fa fa-home', '1', '2018-09-18 14:00:02', '2018-09-18 14:00:02');
+INSERT INTO `sys_resource` VALUES ('2', '2', '权限管理', '权限管理', '', '0', '2', '0', '', 'fa fa-home', '1', '2018-09-18 14:00:02', '2018-09-18 14:00:02');
 
 -- ----------------------------
 -- Table structure for sys_role
@@ -50,16 +51,17 @@ CREATE TABLE `sys_role` (
   `role_uuid` varchar(20) NOT NULL COMMENT '装逼的id',
   `role` varchar(100) NOT NULL COMMENT '角色名称',
   `description` varchar(100) NOT NULL COMMENT '角色描述',
-  `resource_ids` varchar(100) NOT NULL COMMENT '授权的资源',
-  `available` int(1) NOT NULL COMMENT '是否可用：1有效2删除',
+  `status` int(1) NOT NULL COMMENT '是否可用：1有效2删除',
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   `update_time` datetime DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`roleId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of sys_role
 -- ----------------------------
+INSERT INTO `sys_role` VALUES ('1', '1', '超级管理员', '超级管理员', '1', '2018-09-18 14:00:02', '2018-09-18 14:00:02');
+INSERT INTO `sys_role` VALUES ('2', '2', '管理员', '管理员', '1', '2018-09-18 14:00:02', '2018-09-18 14:00:02');
 
 -- ----------------------------
 -- Table structure for sys_role_resource
@@ -70,11 +72,13 @@ CREATE TABLE `sys_role_resource` (
   `roleId` varchar(20) NOT NULL COMMENT '角色id',
   `resourceId` varchar(20) NOT NULL COMMENT '资源id',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of sys_role_resource
 -- ----------------------------
+INSERT INTO `sys_role_resource` VALUES ('1', '1', '1');
+INSERT INTO `sys_role_resource` VALUES ('2', '1', '2');
 
 -- ----------------------------
 -- Table structure for sys_user
@@ -86,21 +90,21 @@ CREATE TABLE `sys_user` (
   `username` varchar(100) NOT NULL COMMENT '用户名',
   `password` varchar(100) NOT NULL COMMENT '密码',
   `salt` varchar(50) DEFAULT NULL COMMENT '盐',
-  `role_ids` varchar(100) NOT NULL COMMENT '角色列表',
-  `locked` int(1) NOT NULL COMMENT '账户是否锁定:1木有；2你猜啊',
   `email` varchar(50) DEFAULT NULL COMMENT '邮箱',
   `phone` varchar(50) DEFAULT NULL COMMENT '联系方式',
   `sex` int(1) DEFAULT NULL COMMENT '性别：1男2女3变态',
   `age` int(3) DEFAULT NULL COMMENT '年龄',
   `status` int(1) NOT NULL COMMENT '用户状态：1有效2删除',
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '修改时间',
   `last_login_time` datetime DEFAULT NULL COMMENT '最后登陆时间',
   PRIMARY KEY (`userId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of sys_user
 -- ----------------------------
+INSERT INTO `sys_user` VALUES ('1', '1', 'admin', '872359cc44c637cc73b7cd55c06d95e4', '8cd50474d2a3c1e88298e91df8bf6f1c', '523179414@qq.com', '187888899991', '1', '22', '1', '2018-09-18 14:00:02', '2018-09-18 14:00:02', '2018-09-18 14:00:02');
 
 -- ----------------------------
 -- Table structure for sys_user_role
@@ -111,8 +115,9 @@ CREATE TABLE `sys_user_role` (
   `userId` varchar(20) NOT NULL COMMENT '用户id',
   `roleId` varchar(20) NOT NULL COMMENT '角色id',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of sys_user_role
 -- ----------------------------
+INSERT INTO `sys_user_role` VALUES ('1', '1', '1');
--- src/main/java/com/ad/core/hope/mapper/second/TestMapper.java
+++ src/main/java/com/ad/core/hope/mapper/second/TestMapper.java
@@ -1,15 +1,9 @@
 package com.ad.core.hope.mapper.second;
 
-import com.ad.core.hope.enums.TestUserEnum;
 import com.ad.core.hope.model.admin.SysUser;
-import com.ad.core.hope.util.MyMapper;
-import com.ad.core.hope.vo.base.TestVo;
 import org.apache.ibatis.annotations.Result;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
-import org.springframework.stereotype.Service;
-import tk.mybatis.mapper.common.Mapper;
-
 import java.util.List;
 
 /**
