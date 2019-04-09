c9b54917cd9f6378c4ed770dbb050f85e5357595
qq18281714982 <java@aodeng.cc>
2019-04-09 22:05:52 +0800

    :art: RabbitMQ 学习

---
+++ springboot16-rabbitmq/src/main/java/com/hope/Springboot16RabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.hope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot16RabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot16RabbitmqApplication.class, args);
+	}
+
+}
