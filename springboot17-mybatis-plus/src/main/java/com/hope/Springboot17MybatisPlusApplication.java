08c5fe65db01293603651fdb6586f203edfecca7
王王梦炜 <1257441793@qq.com>
2019-04-24 17:41:53 +0800

    mybatis -plus

---
+++ springboot17-mybatis-plus/src/main/java/com/hope/Springboot17MybatisPlusApplication.java
@@ -0,0 +1,13 @@
+package com.hope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot17MybatisPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot17MybatisPlusApplication.class, args);
+    }
+
+}
---
+++ springboot17-mybatis-plus/src/main/java/com/hope/generator/GengeratorTest.java
@@ -0,0 +1,13 @@
+package com.hope.generator;
+
+/**
+ * @program:hope
+ * @ClassName:GengeratorTest
+ * @author:aodeng
+ * @blog:低调小熊猫(https://aodeng.cc)
+ * @create:2019-04-24 14:49
+ * @Description: mybatis-plus 代码生成器
+ * @Version 1.0
+ **/
+public class GengeratorTest {
+}
