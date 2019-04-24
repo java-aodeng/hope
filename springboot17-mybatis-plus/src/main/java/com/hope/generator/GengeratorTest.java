1c8efbbf8296db791a749dd5e3ceb183eb19f597
王王梦炜 <1257441793@qq.com>
2019-04-24 18:58:10 +0800

    mybatis -plus

--- springboot17-mybatis-plus/src/main/java/com/hope/generator/GengeratorTest.java
+++ springboot17-mybatis-plus/src/main/java/com/hope/generator/GengeratorTest.java
@@ -1,5 +1,13 @@
 package com.hope.generator;
 
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+
 /**
  * @program:hope
  * @ClassName:GengeratorTest
@@ -10,4 +18,55 @@ package com.hope.generator;
  * @Version 1.0
  **/
 public class GengeratorTest {
+
+    public void main(String[] args) {
+        String packageName="com.hope.generator";
+        //user -> UserService, 设置成true: user -> IUserService
+        boolean serviceNameStartWithI=false;
+
+        //mybatis-plus generator代码生成入口
+        GengeratorTest.generatorByTables(serviceNameStartWithI,packageName,"SysUser","role");
+    }
+
+    private static void generatorByTables(boolean serviceNameStartWithI,String packageName,String... tableNames){
+        GlobalConfig globalConfig=new GlobalConfig();
+
+        String dbUrl="jdbc:mysql://localhost:3306/hope";
+
+        DataSourceConfig dataSourceConfig=new DataSourceConfig();
+        dataSourceConfig.setDbType(DbType.MYSQL)
+                .setUrl(dbUrl)
+                .setUsername("root")
+                .setPassword("123456")
+                .setDriverName("com.mysql.jdbc.Driver");
+
+        StrategyConfig strategyConfig=new StrategyConfig();
+        strategyConfig.setCapitalMode(true)
+                .setEntityLombokModel(false)
+                .setDbColumnUnderline(true)
+                .setNaming(NamingStrategy.underline_to_camel)
+                .setInclude(tableNames);
+
+        globalConfig.setActiveRecord(true)
+                .setAuthor("低调小熊猫")
+                .setOutputDir("d:\\codeGen")
+                .setFileOverride(true);
+
+        if(!serviceNameStartWithI){
+            globalConfig.setServiceName("%sService");
+        }
+
+        new AutoGenerator().setGlobalConfig(globalConfig)
+                .setDataSource(dataSourceConfig)
+                .setStrategy(strategyConfig)
+                .setPackageInfo(
+                        new PackageConfig().setParent(packageName)
+                        .setController("controller")
+                        .setEntity("entity")
+                ).execute();
+    }
+
+    private static void generatorByTables(String packageName,String... tableNames){
+        generatorByTables(true,packageName,tableNames);
+    }
 }
